6c11eb2d72e3f57e2651191c19cbff45f77887d1
ethanielgarza <eagarza@ualr.edu>
2025-08-27 19:32:15 +0000

    update

--- 01.09 Miles In a Kilometer.py
+++ 01.09 Miles In a Kilometer.py
@@ -1,4 +1,6 @@
 kilometers_str = float(input("Enter Kilometers: "))
-conversion_factor = 1.61
-miles = conversion_factor // kilometers_str
+#conversion factor
+conv_fac = 1.61
+miles = kilometers_str // conv_fac
+print("ilometers_str is eqaul to miles")
 
