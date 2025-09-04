1c7fd60fba680ed299bcbee8ee91aaab0b3ff5ee
ethanielgarza <eagarza@ualr.edu>
2025-09-04 17:12:37 +0000

    update

--- 03.01 Minimum of Three Numbers.py
+++ 03.01 Minimum of Three Numbers.py
@@ -1,4 +1,10 @@
 first = int(input("Enter Frist Number: "))
 second = int(input("Enter Second Number: "))
 third = int(input("Enter Third Number: "))
-    if < first
+if first <= second and first <= third:
+    leastamount = first 
+elif second <= first and second <= third:
+    leastamount = second
+else:
+    leastamount = third
+print(leastamount)
---
+++ 03.02 Equal Numbers.py
@@ -0,0 +1,6 @@
+first = int(input("Enter Frist Number: "))
+second = int(input("Enter Second Number: "))
+third = int(input("Enter Third Number: "))
+if first == second and second == third:
+    print(first)
+    
