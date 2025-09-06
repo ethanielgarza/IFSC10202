3a5240c162054859f8a7389de5669cfe99cfe54b
ethanielgarza <eagarza@ualr.edu>
2025-09-06 11:01:17 +0000

    update

--- 02.Project.py
+++ 02.Project.py
@@ -6,16 +6,20 @@ end1 = float(input("Ending point - Enter Latitude: "))
 end2 = float(input("Ending Point - Enter Longitude: "))
 
 import math
-degrees1 = math.radians(point1)
-degrees2 = math.radians(point2)
-degrees3 = math.radians(end1)
-degrees4 = math.radians(end2)
 
-dif1 = degrees4 - degrees2
-dif2 = degrees3 - degrees1
 
-a = math.sin(dif2 / 2)**2 + math.cos(degrees1) * math.cos(degrees3) * math.sin(dif1 / 2)**2
+lat1 = math.radians(point1)
+long1 = math.radians(point2)
+lat2 = math.radians(end1)
+long2 = math.radians(end2)
+
+dif1 = long2 - long1
+dif2 = lat2 - lat1
+
+a = math.sin(dif2 / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dif1 / 2)**2
 c = 2 * math.asin(math.sqrt(a))
 distance = radius * c 
 
-print("The Great Circle Distance:", distance )
+number = distance
+roundnumber = round(number, 2)
+print('The Great Circle Distance:', roundnumber)
