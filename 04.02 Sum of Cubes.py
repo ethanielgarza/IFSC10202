a1d8dcbc7da60c6a2c57d535c67aab6454a7c846
ethanielgarza <eagarza@ualr.edu>
2025-09-15 15:27:25 +0000

    update

--- 04.02 Sum of Cubes.py
+++ 04.02 Sum of Cubes.py
@@ -1,9 +1,3 @@
-def sumcube(n):    #defining the function
-    sum = 0         #constant
-    while(n > 0):   #checking the condition
-        x = n % 10  #will result the remainder
-        sum = sum + (pow(x,3))  #3 is the power of the cube
-        n = n // 10 #result in the qoutient
-    print("Sum of cubes: ", sum)
 n = int(input("Enter Number: "))
-sumcube(n)
+sum = 0
+for i in range(1,n+1)
