9232171a708045648dd84ef9f65b5bfbc8abca17
ethanielgarza <eagarza@ualr.edu>
2025-09-23 18:15:50 +0000

    update

---
+++ 06.01 Circle.py
@@ -0,0 +1,9 @@
+def diameter(radius):
+    return 2 * radius
+import math
+def circumfrence (radius):
+    circumfrence = 2 * math.pi * radius
+    return circumfrence
+def area(radius):
+    area = math.pi *(radius ** 2)
+    return area
