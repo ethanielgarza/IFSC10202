81ea3bae2c2bcbc80f35976d13e9b7755bb37846
ethanielgarza <eagarza@ualr.edu>
2025-10-01 14:13:21 +0000

    update

--- 06.Project.py
+++ 06.Project.py
@@ -8,15 +8,17 @@ output_records = 0
 
 with open(input_filename, 'r') as infile, open(output_filename, 'w') as outfile:
     for line in infile:
-        input_records += 1
-        output_records += 1
-        outfile.write(line)
         if "**Insert Merge File Here**" in line:
             with open(merge_filename, 'r') as mergefile:
                 for merge_line in mergefile:
                     merge_records += 1
-                    output_records += 1 
+                    output_records += 1
                     outfile.write(merge_line);
+            outfile.write("\n")
+        else:
+            output_records += 1
+            input_records += 1
+            outfile.write(line);
 print(f"{input_records} input file records")
 print(f"{merge_records} merge file records")
 print(f"{output_records} output file records")
