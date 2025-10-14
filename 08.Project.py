3d1f86aec50727f06ec7af5d66d90f2e26af00a3
ethanielgarza <eagarza@ualr.edu>
2025-10-14 17:45:12 +0000

    update

--- 08.Project.py
+++ 08.Project.py
@@ -1,25 +1,18 @@
 def find_sections(search_term):
     with open("constitution.txt", "r") as file:
         lines = file.readlines()
-    found_lines = []
     for i, line in enumerate(lines):
             if search_term.lower() in line.lower():
                 start = i
-                while start > 0 and lines[start-1].strip() != "":
+                while start > 0 and lines[start -1].strip() != "":
                     start -= 1
                 end = i 
                 while end < len(lines) and lines[end].strip() != "":
                     end += 1
-#                found_lines.append(lines[start:end])
-#    printed_sections = set()
+
                 for j in range(start,end):
                     print("Line:", j, lines[j].strip())
-                print("#######")
-#        if (start, end) not in printed_sections:
-#             printed_sections.add((start, end))
-#             for j in range(start, end):
-#                  print(f"line {j+1}: {lines[j].strip()}")
-#        print()
+                print()
 def main():
      while True:
           search_term = input("Enter search term: ")
