0f2695a9fdef2a8adb6d6d52dd0bee359556949c
ethanielgarza <eagarza@ualr.edu>
2025-10-29 14:57:54 +0000

    update

--- 10.01 Pets.py
+++ 10.01 Pets.py
@@ -1,5 +1,45 @@
-class Pet():
+class Pet:
+    # Attributes
+    Name = ""
+    Type = ""
+    Age = 0
+
+    # No-argument constructor
     def __init__(self):
-        self.Name = ""
-        self.Type = ""
-        self.Age = 0
+        pass
+
+    def set_name(self, name):
+        self.Name = name
+
+    def set_type(self, pet_type):
+        self.Type = pet_type
+
+    def set_age(self, age):
+        self.Age = age
+
+# Function to read file, create objects, and print attributes
+def main():
+    pets = []
+    with open("10.01 Pets.txt", "r") as file:
+            for line in file:
+                data = line.strip().split(", ")
+                if len(data) == 3:
+                    pet = Pet()
+                    pet.set_name(data[0])
+                    pet.set_type(data[1])
+                    pet.set_age(int(data[2]))
+                    pets.append(pet)
+
+    # Print the header
+    print("{:<10} {:<10} {:<10}".format("Name", "Type", "Age"))
+
+    # Print attributes of each pet object
+    for pet in pets:
+        print("{:<10} {:<10} {:<10}".format(pet.Name, pet.Type, pet.Age))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
--- 10.02 Retail Item.py
+++ 10.02 Retail Item.py
@@ -2,7 +2,6 @@
 class RetailItem():
 # Define Initializer
     def __init__(self, Description ="" , UnitsOnHand = 0, Price = 0):
-            
             # Define Attributes
             self.Description = Description
             self.UnitsOnHand = UnitsOnHand
@@ -12,6 +11,21 @@ class RetailItem():
         InventoryValue = self.UnitsOnHand * self.Price
         return InventoryValue
 # Read File to get values and then print them out in Terminal
-open("10.02 Inventory.txt", "r") 
-
-
+def main():
+    Inventory_items = []
+    with open("10.02 Inventory.txt", "r") as file:
+        for line in file:
+            data = line.strip().split(", ")
+            if len(data) == 3:
+                Description = data[0]
+                UnitsOnHand = int(data[1])
+                Price = float(data[2])
+                item = RetailItem(Description, UnitsOnHand, Price)
+                Inventory_items.append(item)
+    print(f"{"Description":<20}{"Units on Hand":<20}{"Price":<20}{"Inventory Value":<20}")
+    print("-" * 70)
+    for item in Inventory_items:
+        InventoryValue = item.InventoryValue()
+        print(f"{item.Description:<20}{item.UnitsOnHand:<20}{item.Price:<10.2f}{InventoryValue:>20.2f}")
+if __name__ == "__main__":
+     main()
