8ec43d0dc8646ebad6845edd8e10a36ae8f559d8
ethanielgarza <eagarza@ualr.edu>
2025-11-13 19:02:37 +0000

    update

---
+++ Final Project.py
@@ -0,0 +1,11 @@
+class Employee:
+    
+    def __init__ (self, employeenumber, firstname, lastname, address, city, state, zip):
+
+        self.EmployeeNumber = employeenumber
+        self.Firstname = firstname
+        self.Lastname = lastname
+        self.Address = address
+        self.City = city
+        self.State = state
+        self.Zip = zip
