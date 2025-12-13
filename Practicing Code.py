3be0d82b4b9aa2938628338dd91a0f5c26ac3f90
ethanielgarza <eagarza@ualr.edu>
2025-12-13 18:42:35 +0000

    update

---
+++ Practicing Code.py
@@ -0,0 +1,17 @@
+#print("o----""\n")
+#print(" ||||")
+#print("Jesus is KING\n")
+#print('*' * 15) #Expression
+
+#Variables
+price = 10
+rating = 2.5
+name = "Reacher"
+#divison = price // 5
+print(price)
+is_published = False
+
+# Hostiptal 
+Name = 'John Smith'
+age = 20
+Patient_status = True
