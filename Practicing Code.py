6c6a2ca91244a8ec56e0a0825a9b7d24f3a817cc
ethanielgarza <eagarza@ualr.edu>
2026-01-23 00:32:48 +0000

    update

--- Practicing Code.py
+++ Practicing Code.py
@@ -1,25 +1,3 @@
-#print("o----""\n")
-#print(" ||||")
-#print("Jesus is KING\n")
-#print('*' * 15) #Expression
-
-#Variables
-#price = 10
-#rating = 2.5
-#name = "Reacher"
-#divison = price // 5
-#print(price)
-#is_published = False
-
-# Hostiptal 
-#Name = 'John Smith'
-#age = 20
-#Patient_status = True
-
-
-#print("A journey begins with faliure, and with ever faliure, becomes a greater man!")
-
-
 print('Winners and Losers - Human is Even, Computer is Odd')
 
 humanscore = 0
